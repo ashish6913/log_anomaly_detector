98d871fe6dc056499d9a6bd2e69d202bff5aa23b
Ashish Kumar <Ashish.Kumar@cra-arc.gc.ca>
2022-01-31 21:28:55 +0000

    Troubleshooting

--- movie-client/src/environments/environment.ts
+++ movie-client/src/environments/environment.ts
@@ -21,7 +21,7 @@ export const environment = {
       exportTimeoutMillis: '30000', // How long the export can run before it is cancelled
     },
     otelcolConfig: {
-      url: 'jaeger-streaming-collector-headless:14268', //URL of opentelemetry collector
+      url: 'http://jaeger-streaming-collector-headless:14268', //URL of opentelemetry collector
       // url: 'http://localhost:55681/v1/traces', //URL of opentelemetry collector
       
     },
