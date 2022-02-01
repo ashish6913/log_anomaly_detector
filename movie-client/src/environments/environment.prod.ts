bc845d679e12732c608567c2b6a800bd55f4cbe7
Ashish Kumar <Ashish.Kumar@cra-arc.gc.ca>
2022-02-01 20:26:07 +0000

    Update otel url

--- movie-client/src/environments/environment.prod.ts
+++ movie-client/src/environments/environment.prod.ts
@@ -18,7 +18,7 @@ export const environment = {
       exportTimeoutMillis: '30000', // How long the export can run before it is cancelled
     },
     otelcolConfig: {
-      url: 'http://otel-collector-headless.lad-poc.svc.cluster.local:4317/v1/traces', //URL of opentelemetry collector
+      url: 'http://otel-collector-headless:4317/v1/traces', //URL of opentelemetry collector
       
     },
     jaegerPropagatorConfig: {
