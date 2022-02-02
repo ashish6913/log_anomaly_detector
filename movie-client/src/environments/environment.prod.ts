6f27aca50a51b61012ec0a11d131f9959f2e1b92
Ashish Kumar <Ashish.Kumar@cra-arc.gc.ca>
2022-02-02 15:36:53 +0000

    Update otel port

--- movie-client/src/environments/environment.prod.ts
+++ movie-client/src/environments/environment.prod.ts
@@ -18,7 +18,7 @@ export const environment = {
       exportTimeoutMillis: '30000', // How long the export can run before it is cancelled
     },
     otelcolConfig: {
-      url: '172.30.98.88:55681/v1/traces'
+      url: 'http://otel2-collector-headless:55681/v1/traces'
       // url: '172.30.22.104:4317/v1/traces', //URL of opentelemetry collector
       
     },
