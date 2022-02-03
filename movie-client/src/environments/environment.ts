1135ff101a17583f63fe7f7fef6b78a8859d8704
Ashish Kumar <Ashish.Kumar@cra-arc.gc.ca>
2022-02-03 14:36:08 +0000

    Update environment file

--- movie-client/src/environments/environment.ts
+++ movie-client/src/environments/environment.ts
@@ -28,7 +28,7 @@ export const environment = {
     // },
     otelcolConfig: {
       // url: 'http://otel2-collector-headless:4318/v1/traces'
-      url: '10.131.0.59:4318'
+      // url: '10.131.0.59:4318'
        // url: '172.30.22.104:4317/v1/traces', //URL of opentelemetry collector
        
      },
